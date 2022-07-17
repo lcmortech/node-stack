d7e52b8f523c1c3f2dc85cd28b78fb45b28ca819
Lauren Morgan <rg326@live.com>
2022-07-16 23:25:02 -0400

    added file test-age.ts

---
+++ hands-on-typescript/ch1/test-age.js
@@ -0,0 +1,9 @@
+function canDrive(user) {
+    console.log("user is " + user.name);
+    if (user.age >= 16) {
+        console.log('You can drive!');
+    }
+    else {
+        console.log('No driving for you!');
+    }
+}
