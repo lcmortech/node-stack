fb43eead8cefbf559a9adb66ff3700e874871045
Lauren Morgan <rg326@live.com>
2022-07-17 21:55:35 -0400

    added user interface for test-age.ts

--- hands-on-typescript/ch1/test-age.ts
+++ hands-on-typescript/ch1/test-age.ts
@@ -1,15 +1,37 @@
-function canDrive(user){
-    console.log(`user is ${user.name}`)
+// function canDrive(user){
+//     console.log(`user is ${user.name}`)
 
-    if(user.age >= 16){
-        console.log('You can drive!')
-    } else {
-        console.log('No driving for you!')
-    }
+//     if(user.age >= 16){
+//         console.log('You can drive!')
+//     } else {
+//         console.log('No driving for you!')
+//     }
+// }
+
+// const tom ={
+//     name: 'tom',
+//     age: 18
+// }
+// canDrive(tom)
+
+interface User {
+    name: string,
+    age: number
 }
 
-const tom ={
+function canDrive(usr: User) {
+    console.log(`User is ${usr.name}`)
+
+    if(usr.age >= 16) {
+        console.log(`Allow to drive`);
+    } else {
+        console.log(`Do not allow to drive`);
+    }   
+}
+
+const tom = {
     name: 'tom',
     age: 18
 }
-canDrive(tom)
+canDrive (tom);
+//ts will complain if age not added, as per User interface requirements
